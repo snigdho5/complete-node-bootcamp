19242f8b3bf95b2258b3471adcc2ecebe30b9340
Snigdho Upadhyay <iupadhyay.snigdho@gmail.com>
2021-04-21 23:54:12 +0530

    today

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -4,14 +4,19 @@ const fs = require('fs');
 const name = 'hello world '+ between(10, 200);
 console.log(name);
 
-const fileInp = fs.readFileSync('txt/input.txt', 'utf-8');
+//sync
+// const fileInp = fs.readFileSync('txt/input.txt', 'utf-8');
 
-console.log(fileInp);
+// console.log(fileInp);
 
-const fileOut = `this is a str concat with file str: ${fileInp}.\nCreated: ${Date.now()}`;
-fs.writeFileSync('txt/output.txt', fileOut);
-console.log('file written with'+fileOut);
+// const fileOut = `this is a str concat with file str: ${fileInp}.\nCreated: ${Date.now()}`;
+// fs.writeFileSync('txt/output.txt', fileOut);
+// console.log('file written with'+fileOut);
 
+//async
+fs.readFile('txt/input.txt', 'utf-8', (err, data)=>{
+    console.log(data);
+});
 
 // func
 function between(min, max) {  
